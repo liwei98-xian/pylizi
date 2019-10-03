260d25bcd247cf49edaf0b21691ac84634cda99f
liwei <1781855437@qq.com>
2019-10-03 10:36:11 +0800

    s1

--- b.py
+++ b.py
@@ -1,6 +1,6 @@
-a='*'
-n=int(input('请输入一个数字：'))
-for i in range(1,n,2):
-    print((i*'*').center(n-1))
-for i in reversed(range(1,n-2,2)):
-    print((i*a).center(n-1))
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(str(i)+'*'+str(j)+'='+str(i*j),end='      ')
+    print(i)
+    print('')
+    
